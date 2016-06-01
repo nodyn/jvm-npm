8d70a2b7ef38f4be369cb5e9a68f67871efb914c
bsorrentino <bartolomeo.sorrentino@gmail.com>
2016-06-01 18:29:40 +0200

    add ignore feature

--- src/main/javascript/jvm-jasmine.js
+++ src/main/javascript/jvm-jasmine.js
@@ -1,7 +1,7 @@
 /**
- * 
- * JASMINE JS LITE FOR JVM JAVASCRIPT 
- * 
+ *
+ * JASMINE JS LITE FOR JVM JAVASCRIPT
+ *
  */
 
 var writer = new java.io.StringWriter();
@@ -12,17 +12,17 @@ var System = java.lang.System,
 
 var jasmine = {
     nTest:0,
-    nFail:0,  
+    nFail:0,
     beforeEach:null,
     printReport:function() {
         out.println();
         out.println("========================");
         out.print( "report test/fail ");
-            out.print(new Integer(jasmine.nTest)); 
-            out.print("/"); 
+            out.print(new Integer(jasmine.nTest));
+            out.print("/");
             out.println(new Integer(jasmine.nFail));
         out.println("========================");
-        out.println();       
+        out.println();
     }
 };
 
@@ -30,104 +30,108 @@ function beforeEach( cb ) {
     jasmine.beforeEach = cb;
 }
 
-function describe( msg, cb ) {
-   
-    out.println( msg );
-   
-    if( jasmine.beforeEach ) jasmine.beforeEach();
-    
-    cb();
-}
-
-function it( msg, cb ) {
-    ++jasmine.nTest;
-    
+function describe( msg, cb, disable ) {
     this.fail = function( m ) {
         throw new Error(m);
     };
-    
+
+    this.disable = ( disable || false  );
+
+    out.println( msg );
+
+    if( jasmine.beforeEach ) jasmine.beforeEach();
+
+    cb.call(this);
+}
+
+function it( msg, cb, disable ) {
+
+    if(disable === undefined && this.disable ) return
+    if( disable ) return;
+
+    ++jasmine.nTest;
+
     out.print( "\t"); out.print(msg);
-    
+
     try {
-        cb(); 
+        cb();
         out.println(" ....passed");
     }
     catch( e ) {
         ++jasmine.nFail;
-        out.println(" ....error\n\n>>>"); 
-            out.println(e.message); 
+        out.println(" ....error\n\n>>>");
+            out.println(e.message);
             out.println("<<<");
     }
-
 }
 
 function expect( condition ) {
-  
+
     function _compareArray( a, b ) {
         if( a.length!=b.length ) return false;
-        
+
         var i = 0;
-        a.forEach( function(e) {          
+        a.forEach( function(e) {
             if( e != b[i++] ) return false;
         });
         return true;
     }
 
-    
+
     function _toBeTruthy() {
-            
+
             if( condition ) return;
-            
-            var msg =  "expect true but is false" ;            
+
+            var msg =  "expect true but is false" ;
             throw new Error(msg);
-            
+
     }
     function _toBeFalsy() {
-            
+
             if( !condition ) return;
-            
-            var msg =  "expect false but is true" ;            
+
+            var msg =  "expect false but is true" ;
             throw new Error(msg);
-            
+
     }
     function _toBeNull() {
-            
+
             if( condition==null ) return;
-            
-            var msg =  "expect null but is " + condition ;            
+
+            var msg =  "expect null but is " + condition ;
             throw new Error(msg);
-            
+
     }
     function _toBeNotNull() {
             print( "\n_toBeNotNull", condition );
             if( condition!=null ) return;
-            
-            var msg =  "expect not null but is null" ;            
+
+            var msg =  "expect not null but is null" ;
             throw new Error(msg);
-            
+
     }
     function _toBeDefined() {
         if( condition != undefined ) return;
 
-        var msg =  "expect defined but is undefined" ;            
-        throw new Error(msg);            
+        var msg =  "expect defined but is undefined" ;
+        throw new Error(msg);
     }
     function _toBeUndefined() {
         if( condition == undefined ) return;
 
-        var msg =  "expect undefined but is " + condition ;            
-        throw new Error(msg);            
+        var msg =  "expect undefined but is " + condition ;
+        throw new Error(msg);
     }
 
-    return { 
-        
+    return {
+
         not: {
             toBeTruthy:function() {
                 return _toBeFalsy();
             },
             toBeFalsy:function() {
                 return _toBeTruthy();
-            },  
+            },
             toBeNull:function() {
                 return _toBeNotNull();
             },
@@ -140,8 +144,8 @@ function expect( condition ) {
         },
         toBe:function(c) {
             if( c == condition ) return;
-            
-            var msg =  "expect ["+ c + "] but is ["+condition+"]" ;              
+
+            var msg =  "expect ["+ c + "] but is ["+condition+"]" ;
             throw new Error(msg);
         },
         toBeTruthy:function() {
@@ -149,53 +153,53 @@ function expect( condition ) {
         },
         toBeFalsy:function() {
             _toBeFalsy();
-        },           
+        },
         toThrow:function(c) {
             try {
-                condition();                
+                condition();
             }
             catch(ex) {
                 if( ex.message != c.message ) {
-                    var msg =  "expect ["+c.message+"] exception but is ["+ex.message+"]" ;            
-                    throw new Error(msg);                    
+                    var msg =  "expect ["+c.message+"] exception but is ["+ex.message+"]" ;
+                    throw new Error(msg);
                 }
                 return;
             }
-            
-            var msg =  "expect exception but none" ;            
+
+            var msg =  "expect exception but none" ;
             throw new Error(msg)
         },
         toBeDefined:function() {
             _toBeDefined();
         },
         toEqual:function(c) {
-            
+
             if( typeof c != typeof condition ) {
-                throw new Error("exspected type ["+(typeof c)+"] is different  from given ["+(typeof condition)+"]" );            
-            }   
-            
+                throw new Error("exspected type ["+(typeof c)+"] is different  from given ["+(typeof condition)+"]" );
+            }
+
             if( typeof c == 'object') {
                 if( _compareArray( c, condition ) ) return;
             }
-            
+
             if( c == condition ) {
                 return;
             }
-            
-            var msg =  "expect ["+c+"] exception but is ["+condition+"]" ;            
-            throw new Error(msg);            
+
+            var msg =  "expect ["+c+"] exception but is ["+condition+"]" ;
+            throw new Error(msg);
         }
     }
 }
 
 
 function report() {
-    
+
     jasmine.printReport();
-    
+
     out.flush();
     System.out.println( writer.toString() );
-    
+
     if( jasmine.nFail > 0 ) {
         throw new Error( "TEST FAILED!");
     }
